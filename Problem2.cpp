a4cd572cdf598ef1128b7a1c66629b35f2581aa2
Shazmaan <shazmaan@buffalo.edu>
2019-08-02 22:33:28 -0400

    Updated Problem2 with a little faster solution.

--- Problem2.cpp
+++ Problem2.cpp
@@ -16,7 +16,7 @@ using namespace std;
 
 class Solution{
 public:
-    string decodeString(string s) {
+    string decodeString(string s) {  //simpler and avoids edge cases.
         if(s == ""){return "";}
 
         stack<int> integer;
@@ -53,5 +53,40 @@ public:
             characters.pop();
         }
         return tempstr;
+    }
+
+    string decodeString(string s) { // a little faster.
+        if(s==""){return "";}
+
+        stack<int> integer;
+        stack<string> str;
+        str.push("");
+
+        string inte = ""; string alp = "";
+        for(int x = 0; x<s.size(); x++){
+            if(isdigit(s[x])){
+                inte += s[x];
+                if(x+1<s.size() && !isdigit(s[x+1])){
+                    integer.push(stoi(inte)); inte = "";
+                    str.push("");
+                }
+            }else if(isalpha(s[x])){
+                alp += s[x];
+                if((x+1<s.size() && !isalpha(s[x+1])) || x+1>=s.size()){
+                    string stop = str.top(); str.pop();
+                    str.push(stop+alp); alp = "";
+                }
+            }else if(s[x]==']'){
+                int inttop = integer.top(); integer.pop();
+                string strtop = str.top(); str.pop();
+                string alpha = strtop;
+                for(int i = 1; i<inttop; i++){
+                    strtop = alpha +strtop;
+                }
+                alpha = str.top(); str.pop();
+                str.push(alpha+strtop);
+            }
+        }
+        return str.top();
     }
 };
