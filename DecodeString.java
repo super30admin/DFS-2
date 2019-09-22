88d17c7ecb5b72542f8b464feec4d9d1f28e71d9
Akhilesh Borgaonkar <aborgaonkar@onemarketnetwork.com>
2019-09-22 12:02:54 -0700

    DecodeString problem issues resolved

--- DecodeString.java
+++ DecodeString.java
@@ -1,44 +1,46 @@
 /*
 Author: Akhilesh Borgaonkar
-Known Issue: in looping square brackets, prints output in reverse manner.
+Approach: Recursive approach here. Read the character literals sequentially and act accordingly. If number detected, store it temporary. 
+	If '[' detected, recursive call to parse the substring inside square brackets and append to current string. 
+	If ']' detected then return the current string. 
+	If alphabet detected, append to output string.
+Time complexity: O(n)
+Space complexity: O(n) for string builder
+Issue previously faced: in looping square brackets, prints output in reverse manner. (Resolved by considering multiple digits and alphabets in string)
 */
-
-import java.util.ArrayList;
-import java.util.Stack;
 
 public class DecodeString {
 
+     static int i = 0;
+
     public static String decodeString(String s) {
 
-        Stack<Integer> frequency = new Stack<Integer>();
-        Stack<String> strings = new Stack<String>();
-        String currStr;
-        String output = "";
+        StringBuilder output = new StringBuilder();
         int currFreq=0;
 
-        for(int i=0; i<s.length() ; i++){
-            if(Character.isDigit(s.charAt(i))){
-                currFreq = Character.getNumericValue(s.charAt(i));
-                System.out.println(currFreq);
-            }
-            else if(s.charAt(i)=='[') {
-                frequency.push(currFreq);
+        while(i < s.length()){
+            char now = s.charAt(i);
 
-                currStr="";
+            if(now >= '0' && now <= '9'){
+                currFreq = currFreq*10 + now - '0';
+            }
+            else if(now=='[') {
+                i++;
+                String next = decodeString(s);
+                for(int count = 0; count < currFreq; count++)
+                    output.append(next);
                 currFreq=0;
             }
-            else if(s.charAt(i)==']') {
-                currStr = strings.pop();
-                for(int j = frequency.pop(); j>0; --j)
-                    output+=currStr;
-                System.out.println(output);
+            else if(now==']') {
+                return output.toString();
             }
             else {
-                strings.push(Character.toString(s.charAt(i)));
+                output.append(now);
             }
+            i++;
         }
 
-        return output;
+        return output.toString();
     }
 
 }
