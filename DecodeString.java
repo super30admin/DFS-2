75957a65e9f9409c12587b91b9db5d4087e27a1f
keerthitejakonuri <keerthi49100@gmail.com>
2019-11-08 20:56:22 -0600

    Update DecodeString.java
    recursive solution

--- DecodeString.java
+++ DecodeString.java
@@ -6,6 +6,39 @@
 
 // Your code here along with comments explaining your approach
 class DecodeString {
+	int index = 0, n;
+    public String decodeString2(String s){
+        if(null == s || 0 == s.length()){return "";}
+        n = s.length();  
+        return helper(s,new StringBuilder("")).toString();
+    }
+    public StringBuilder helper(String s, StringBuilder result){
+        if(index >= n){
+            return result;
+        }
+            if(Character.isDigit(s.charAt(index))){
+                int count = 0;
+                while(index < n && Character.isDigit(s.charAt(index))){
+                    count = count*10 + s.charAt(index) - '0';
+                    index++;
+                }
+                StringBuilder temp = helper(s,new StringBuilder(""));
+                    for(int i = 0 ; i < count; i++){
+                        result.append(temp);
+                    }
+                return result.append(helper(s,new StringBuilder("")));
+            }else if('[' == s.charAt(index)){
+                index++;
+                return helper(s,result);
+            }else if(']' == s.charAt(index)){
+                index++;
+                return result;
+            }else{
+                result.append(s.charAt(index));
+                index++;
+                return helper(s,result);
+            }
+    }
     public String decodeString(String s) {
         StringBuilder res = new StringBuilder("");
         Stack<Integer> countStack = new Stack<Integer>();
