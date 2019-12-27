6be6725ace57e3106a81a4ccf5833909ee479fdd
Rishika Machina <39758648+RishikaMachina@users.noreply.github.com>
2019-12-27 15:07:14 -0800

    Update Problem_1.py

--- Problem_1.py
+++ Problem_1.py
@@ -1,6 +1,6 @@
 # Runs on Leetcode
 
-# DFS solution
+# DFS solution 1
         # Runtime - O(n)
         # Memory - O(1)
 
@@ -29,7 +29,34 @@ class Solution:
         if 0<=j-1<len(self.grid[0]) and self.grid[i][j-1] == '1':
             self.dfs(i,j-1)
             
-    
+# DFS solution 2 
+        # Runtime - O(n)
+        # Memory - O(1)
+                
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+        self.grid = grid
+        self.count = 0
+        self.directions = [[0,1],[0,-1],[1,0],[-1,0]]
+        
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    self.count += 1
+                    self.dfs(i,j)
+        return self.count
+        
+    def dfs(self,i,j):
+        self.grid[i][j] = '0'
+        for d in self.directions:
+            x = i + d[0]
+            y = j + d[1]
+            if 0<=x<len(self.grid) and 0<=y<len(self.grid[0]) and self.grid[x][y] == '1':
+                self.dfs(x,y)
+                
+                
 # BFS solution
         # Runtime - O(n)
         # Memory - O(n)
