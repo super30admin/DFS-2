120f1eab9589bcba9496cf276092a30b84949473
Sweety Sojrani <sweety.sojrani@sjsu.edu>
2020-01-26 08:57:05 -0800

    Update Problem2.java

--- Problem2.java
+++ Problem2.java
@@ -4,22 +4,22 @@
 
 class Solution {
     public String decodeString(String s) {
-        Stack<Integer> numStack = new Stack<>();
-        Stack<String> strStack = new Stack<>();   // To store integer
+        Stack<Integer> numStack = new Stack<>();    // To process characters in the string
+        Stack<String> strStack = new Stack<>();   // To process digit in the string
         int currNum = 0;
         String currStr = "";
-        for(int k =0;k < s.length(); k++){
+        for(int k =0;k < s.length(); k++){      // Iterate over the string to process each character in the string
             char c = s.charAt(k);
             if(Character.isDigit(c)){
-                currNum = currNum * 10 +c -'0';
-            }
-            else if(c == '['){
+                currNum = currNum * 10 +c -'0';     // This will convert the char number to integer and also append to the currNum if it is a multi digit number
+            }   
+            else if(c == '['){              // If it is opening bracket, it means push the number in currNum and string currStr that came before the bracket to the stack to process on closing bracket
                 numStack.push(currNum);
                 strStack.push(currStr);
-                currNum = 0;
+                currNum = 0;    // and reinitialize the currNum and currStr to take new values inside the bracket
                 currStr = "";
             }
-            else if(c == ']'){
+            else if(c == ']'){      // on closing bracket pop the number num from stack and add the currStr num times and add the top of strStack with it.
                 int num = numStack.pop();
                 StringBuilder str = new StringBuilder();
                 for(int i=0; i< num;i++){
@@ -29,9 +29,9 @@ class Solution {
                 
             }
             else{
-                currStr += c; 
+                currStr += c;   // else if it is character add it to the currStr
             }
         }
-        return currStr;
+        return currStr; // return the currStr which will have the resultant string in the end
     }
 }
