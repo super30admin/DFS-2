bc1e05ad7c2cb69bbd548860a542bf05dc413425
Neha Sharma <53287893+neha-0507@users.noreply.github.com>
2020-01-27 13:26:30 -0700

    Update No_islands.py

--- No_islands.py
+++ No_islands.py
@@ -26,5 +26,38 @@ class Solution:
                                 grid[r][c]='0'
                                 q.append((r,c))
         return count
-                   
+    
+    
+    //// DFS
+    class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if len(grid)==0:
+            return 0
+        row=len(grid)
+        col=len(grid[0])
+        count=0
+        self.dir={(0,-1),(-1,0),(0,1),(1,0)}
+        for i in range(row):
+            for j in range(col):
+                if(grid[i][j]=='1'):
+                    count+=1
+                    self.dfs(grid,i,j)
+        return count
+            
+        
+        
+        
+    def dfs(self,grid,i,j):
+        #base
+        if((i<0) or(i>=len(grid)) or(j<0) or (j>=len(grid[0])) or(grid[i][j]=='0')):
+            return
+        
+        
+        
+        #logic
+        grid[i][j]='0'
+        for d in self.dir:
+            r=i+d[0]
+            c=j+d[1]
+            self.dfs(grid,r,c)
             
