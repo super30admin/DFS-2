e6acef28a6d6e10790077d28310f397231639ed0
chirag <cr.tagadiya@gmail.com>
2020-06-28 22:24:17 -0400

    No of island dfs approach

--- No_Of_Island.java
+++ No_Of_Island.java
@@ -49,7 +49,42 @@ public class No_Of_Island {
                 }
             }
         }
+        return count; 
+    }
+
+    /*
+    DFS Solution
+    
+    int m;
+    int n;
+    int [][] dirs = {{0,1},{0,-1},{-1,0},{1,0}};
+    
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0 ) return 0;
+        m = grid.length;
+        n =  grid[0].length;
+        int count=0;
+        for(int i = 0;i < m; i++){
+            for(int j = 0; j < n; j++){
+                if( grid[i][j] == '1'){
+                    count++;
+                    dfs(grid,i,j);       
+                }
+            }   
+        }
         return count;
+    }
+    
+    private void dfs(char[][] grid, int r, int c){
+//         base
+        
+        if( r < 0 || c < 0 || r >=  m || c >= n || grid[r][c] == '0') return;
+        
+        grid[r][c] = '0';
+        for( int[] dir : dirs){
+            dfs(grid, dir[0] + r, dir[1]+c);
+        }
         
     }
+    */
 }
