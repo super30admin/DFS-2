ed9225ef1dca45a2fe94da9f19e1431e9c08b9b8
Arpit Sharma <arpit317@gmail.com>
2020-10-31 14:56:41 -0700

    Update DecodeString.java

--- DecodeString.java
+++ DecodeString.java
@@ -34,3 +34,40 @@ class Solution {
         return currStr.toString();
     }
 }
+
+
+//TC: O(n*k)
+//SC: O(n)
+class Solution {
+    int i = 0;
+    public String decodeString(String s) {
+        StringBuilder currStr = new StringBuilder();
+        int num = 0;
+        if(s == null || s.length() == 0) return currStr.toString();
+        
+        while(i < s.length()){
+            char c = s.charAt(i);
+            if(Character.isDigit(c)){
+                i++;
+                num = num*10 + c - '0';
+            }
+            else if(c == '['){
+                i++;
+                String inner = decodeString(s);
+                for(int k = 0; k < num; k++){
+                    currStr.append(inner);
+                }
+                num = 0;
+            }
+            else if(c == ']'){
+                i++;
+                return currStr.toString();
+            }
+            else{
+                i++;
+                currStr.append(c);
+            }
+        }
+        return currStr.toString();
+    }
+}
