cc1f1bf450e3e6f57bb8dd2e33564578933d5755
tholerohit <76415875+tholerohit@users.noreply.github.com>
2021-01-25 20:10:36 -0500

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,30 @@
+//Time Complexity:O(mn)
+//Space Complexity:O(min(m,n))
+class Solution {
+    int m;
+    int n;
+    public int numIslands(char[][] grid) {
+        if(grid.length==0||grid==null)return 0;
+        m = grid.length;
+        n = grid[0].length;
+        int count = 0;
+        for(int i =0; i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j] == '1'){
+                    count++;
+                    dfs(grid,i,j);
+                }
+            }
+        }return count;
+    }
+    private void dfs(char[][] grid,int i, int j){
+        if(i<0 || j<0 || i == m || j == n || grid[i][j] == '0')return;
+        int [][] dirs = {{0,-1},{0,1},{-1,0},{1,0}};
+        grid[i][j] ='0';
+        for(int [] dir : dirs){
+            int r = dir[0] + i;
+            int c = dir[1] + j;
+            dfs(grid,r,c);
+        }
+    }
+}
