8ba2d69d7303870217db7583c9efd25bb650761d
Kapish Garg <kapish5467@gmail.com>
2021-02-19 09:44:41 -0800

    Completed

--- problem2.java
+++ problem2.java
@@ -1,38 +1,44 @@
-// Time Complexity :O(n)
+// Time Complexity :O(n+ sum of digits)
 // Space Complexity :O(n)
-// Did this code successfully run on Leetcode :no
-// Any problem you faced while coding this :failing test case
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this :No
 
 
 // Your code here along with comments explaining your approach
 class Solution {
     public String decodeString(String s) {
-        String ans="";
-        String temp="";
-        Stack<Character> st = new Stack<>();
+        Stack<Integer> stackInt= new Stack<>();//keeps track of Integer
+        Stack<String> stackChar= new Stack<>();//Keeps track of Character seen so far
+        
+        int num=0;
+        String output="";
+        
         for(int i=0;i<s.length();i++){
-            if(s.charAt(i)==']'){
-                char check=st.pop();
-               String k=String.valueOf(check);
-                check=st.pop();
-                while(check!='['){
-                    k=check+k;
-                    check=st.pop();
-                }
-                temp=k+temp;
-                k=temp;
-                int x= Character.getNumericValue(st.pop());
-                for(int j=1;j<x;j++)
-                    temp=k+temp;
-                ans=ans+temp;
-                temp=ans;
+            Character curr=s.charAt(i);
+            if(Character.isDigit(curr)){
+                num=num*10+(curr-'0');//calculating the number to be lultplied with
             }
-            else{
-                st.push(s.charAt(i));
-                 temp="";
+            else if(curr=='['){//push everything as the braces start
+                stackInt.push(num);
+                stackChar.push(output);
+                
+                num=0;
+                output="";
+                
+            }
+            else if(curr==']'){//pop everythingg and calculate once we see end braces
+                int lim=stackInt.pop();
+                String a="";
+                for(int j=0;j<lim;j++)
+                    a+=output;
+                output=stackChar.pop();
+                output=output+a;
+            }
+            
+            else{//if it alphabet
+                output+=curr;
             }
         }
-        return ans;
-        
+        return output;
     }
 }
