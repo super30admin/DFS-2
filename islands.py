ffdde64884b8b06be40f78e1c64452799a23ddda
alekhyagudi <80299426+alekhyagudi@users.noreply.github.com>
2021-03-19 00:38:38 -0500

    Create islands.py

---
+++ islands.py
@@ -0,0 +1,21 @@
+//Time complexity: o(m*n)
+//space complexity: size of recursive stack (0(m*n)
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        islands = 0
+        for i in range(len(grid)):  
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    islands += 1
+                    self.part_of_island(i,j,grid)
+        return islands
+
+    def part_of_island(self, i, j,grid):
+        if i < 0 or j < 0 or i == len(grid) or j == len(grid[0]) or grid[i][j] != '1':
+            return
+        else:
+            grid[i][j] = '0'
+        self.part_of_island(i,j+1,grid)
+        self.part_of_island(i,j-1,grid)
+        self.part_of_island(i+1,j,grid)
+        self.part_of_island(i-1,j,grid)          
