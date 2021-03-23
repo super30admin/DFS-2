7dbb0bbac858745b82d012e8438104070c5eff58
stuti <stuti@DESKTOP-1KQ9LMO>
2021-03-22 21:19:29 -0400

    done

---
+++ prob66.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        
+        int count = 0;
+        for(int i =0 ; i< grid.length; i++){
+            for(int j =0; j<grid[i].length; j++)  {
+                if(grid[i][j] =='1'){
+                    count += 1;
+                    callBFS(grid,i,j);
+                    
+                }
+        }
+        
+    }
+    
+    return count;
+}
+public void callBFS(char[][]grid,int i, int j)
+{
+    if(i<0 || i>=grid.length || j<0 || j>= grid[i].length|| grid[i][j] == '0')
+        return ;
+    grid[i][j]='0';
+    callBFS(grid, i+1, j );
+    callBFS(grid, i-1, j );
+    callBFS(grid, i, j-1 );
+    callBFS(grid, i, j+1 );
+}
+}
