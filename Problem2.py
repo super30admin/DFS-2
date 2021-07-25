fd38f5d4221e47909d67614bd7737ef66dd5ab82
manojvarmarudraraju <46211625+manojvarmarudraraju@users.noreply.github.com>
2021-07-25 11:12:48 +0530

    Completed Sprint DFS-2 - Please Review

--- Problem2.py
+++ Problem2.py
@@ -1,7 +1,42 @@
-	# Time Complexity : 
-	# Space Complexity : 
-	# Did this code successfully run on Leetcode : 
-	# Any problem you faced while coding this : 
+# Time Complexity : O(n)
+# Space Complexity : O(n)
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : No
+
+#saving currNum in num stack and currStr in string stack and when we encounter ] pop and add to currstring stack
+class Solution:
+    def decodeString(self, s: str) -> str:
+        if s == None or len(s) <= 1:
+            return s
+        nSt = []
+        sSt = []
+        currString = ""
+        num = 0
+        
+        for i in range(len(s)):
+            
+            if s[i].isnumeric():
+                num  = 10*num + int(s[i])
+            
+            elif s[i] == "[":
+                nSt.append(num)
+                sSt.append(currString)
+                num = 0
+                currString = ""
+                    
+            
+            elif s[i] == "]":
+                times = nSt.pop()
+                newStr = ""
+                for j in range(times):
+                    newStr += currString
+                currString = sSt.pop()
+                currString += newStr
+                
+            else:
+                currString += s[i]
+        return currString
+	
 
 
     
