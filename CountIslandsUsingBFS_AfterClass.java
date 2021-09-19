47ec30f75546cae8c51a63bd7413e29ba6ebea3c
sandhya <goswami.sa@northeastern.edu>
2021-09-19 11:10:55 -0400

    Changed Implmentation of CountIsland

---
+++ CountIslandsUsingBFS_AfterClass.java
@@ -0,0 +1,57 @@
+// Time Complexity : O(mn)
+// Space Complexity : min of no of rows or columns in grid
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+
+//1. using BFS 
+//2. go through the entire matrix till you find island , add it into q, check all four sides of this elemment and make 0 
+
+//3.   move and next 1 in q 
+
+class Solution {
+    int m =0;
+    int n = 0;
+    int count =0;
+    
+    int[][] dirs = {{0,1},{0,-1},{1,0},{-1,0}};
+    public int numIslands(char[][] grid) {
+        m = grid.length;
+        n = grid[0].length;
+        if(m ==0 || grid == null) return -1;
+        count =0;
+        Queue<int[]> q = new LinkedList<>();
+        for(int i = 0;  i< m ; i++)
+        {
+             for(int j = 0;  j< n ; j++)
+            {
+                 if(grid[i][j] == '1')
+                 { 
+                     q.add(new int[]{i,j});
+                     count++;
+                    while(!q.isEmpty())
+                    {
+                       grid[i][j] = '0';
+                          int[] curr = q.poll();
+                        for( int[] dir : dirs)
+                        {
+                            int r = curr[0] + dir[0];
+                            int c = curr[1] + dir[1];
+                             if(r < m && r >= 0 && c < n && c >= 0 && grid[r][c] =='1') 
+                           {
+                                grid[r][c] = '0'; 
+                                q.add(new int[]{r,c});
+                            }
+                        } 
+                    }
+                 }
+            }
+        }
+        
+        return count;
+    }
+    
+ 
+}
---
+++ CountIslandsUsingDFS_AfterClass.java
@@ -0,0 +1,51 @@
+// Time Complexity : O(mn)
+// Space Complexity : O(mn)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+
+//1. using DFS 
+//2. go through the entire matrix till you find island call Dfs on that index and increase islandFoundCount by 1
+//3. in DFS call,  make all connected islands = 0 /water 
+
+class Solution {
+    int m =0;
+    int n = 0;
+    int count =0;
+    int[][] dirs = {{0,1},{0,-1},{1,0},{-1,0}};
+    public int numIslands(char[][] grid) {
+        m = grid.length;
+        n = grid[0].length;
+        if(m ==0 || grid == null) return -1;
+        count =0;
+        for(int i = 0;  i< m ; i++)
+        {
+             for(int j = 0;  j< n ; j++)
+            {
+                 if(grid[i][j] == '1')
+                 { 
+                     count++;
+                     dfs(grid, i, j);
+                 }
+            }
+        }
+        
+        return count;
+    }
+    
+    private void dfs(char[][] grid, int i , int j)
+    {
+        if(i == m || i < 0 || j == n || j <0 || grid[i][j] =='0') return;
+        
+        grid[i][j] = '0';
+        
+        for( int[] dir : dirs)
+        {
+            int r = i + dir[0];
+            int c = j + dir[1];
+            dfs(grid, r,c);
+        }
+    }
+}
