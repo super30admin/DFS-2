bbb9aeadf81b201de297c9d2b2248fc328fb9280
Adhish Shrivastava <adhish70@gmail.com>
2021-11-08 13:40:43 -0500

    Initial Commit

---
+++ Problem-66 Number of Islands.py
@@ -0,0 +1,11 @@
+# 200. Number of Islands
+# https://leetcode.com/problems/number-of-islands/
+
+# Logic: 
+
+# Time Complexiety: 
+# Space Complexiety: 
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        pass
---
+++ Problem-67 Decode String.py
@@ -0,0 +1,11 @@
+# 394. Decode String
+# https://leetcode.com/problems/decode-string/
+
+# Logic: 
+
+# Time Complexiety: 
+# Space Complexiety: 
+
+class Solution:
+    def decodeString(self, s: str) -> str:
+        pass
