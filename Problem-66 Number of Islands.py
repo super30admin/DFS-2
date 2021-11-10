16ec292e82129937ec33129ae9ce25a8687b5a30
Adhish Shrivastava <adhish70@gmail.com>
2021-11-10 10:50:39 -0500

    Problem66 and Problem67 done

--- Problem-66 Number of Islands.py
+++ Problem-66 Number of Islands.py
@@ -1,11 +1,35 @@
 # 200. Number of Islands
 # https://leetcode.com/problems/number-of-islands/
 
-# Logic: 
+# Logic: Iterate the matrix. When you find a 1, start BFS and increment the island count.
 
-# Time Complexiety: 
-# Space Complexiety: 
+# Time Complexiety: O(n*m)
+# Space Complexiety: O(min(n,m))
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        pass
+        n = len(grid)
+        m = len(grid[0])
+        islandCount = 0
+        q = list()
+        dirs = [(0,1),(0,-1),(1,0),(-1,0)]
+        
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == '1':
+                    q.append((i,j))
+                    grid[i][j] = '2'
+                    islandCount += 1
+                    
+                    while q:
+                        row, col = q.pop(0)
+                        
+                        for _dir in dirs:
+                            newRow = row + _dir[0]
+                            newCol = col + _dir[1]
+                            
+                            if 0 <= newRow < n and 0 <= newCol < m:
+                                if grid[newRow][newCol] == "1":
+                                    q.append((newRow,newCol))
+                                    grid[newRow][newCol] = "2"
+        return islandCount
--- Problem-67 Decode String.py
+++ Problem-67 Decode String.py
@@ -1,11 +1,41 @@
 # 394. Decode String
 # https://leetcode.com/problems/decode-string/
 
-# Logic: 
+# Logic: Iterate the given string and push into stack everything except closing bracket. 
+# When closing bracket is reached, pop till the opening bracket, and generate the current string. 
+# Pop the integer and make count. Multiply count with current string, and push back to stack.
 
-# Time Complexiety: 
-# Space Complexiety: 
+# Time Complexiety: O(n.maxk) k is the integer count
+# Space Complexiety: O(n)
 
 class Solution:
     def decodeString(self, s: str) -> str:
-        pass
+        stack = list()
+        
+        for i in s:
+            if i != ']':
+                stack.append(i)
+            else:
+                x = stack.pop()
+                temp = [x]
+                count = []
+                
+                while x != '[':
+                    x = stack.pop()
+                    
+                    if x != '[':
+                        temp.append(x)                
+                
+                temp.reverse()
+                
+                while stack and stack[-1].isdigit():
+                    count.append(stack.pop())
+                    
+                count = int("".join(count[::-1]))
+                
+                temp = temp * count
+                
+                for j in temp:
+                    stack.append(j)
+        
+        return "".join(stack)
