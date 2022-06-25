cdee9594af122f4333ac5652d2abc8786633d645
Prasad Gagarani <52399176+prasadgagarani@users.noreply.github.com>
2022-06-25 11:53:31 -0400

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -54,4 +54,68 @@ class Solution {
     }
 }
 
+//****NUMBER OF ISLAND-  BFS APPROACH****
+// Time Complexity : O(m*n);
+// Space Complexity :MIN(m*n)
+// Did this code successfully run on Leetcode :Y
+// Any problem you faced while coding this :N
 // Your code here along with comments explaining your approach
+
+class Solution {
+    int [][] dirs;
+    int m;
+    int n; 
+    public int numIslands(char[][] grid) 
+    {
+        //Boiler plate code for going in 4 different directions
+        m=grid.length;
+        n=grid[0].length;  
+        
+        //Directions array      //u     //D  //L    //R
+        dirs=new int[][]{{-1,0},{1,0},{0,-1},{0,1}};
+        
+        int count=0;
+        //Declaring 2 different queues
+        Queue<Integer> rq=new LinkedList<>();
+        Queue<Integer> cq=new LinkedList<>();
+        
+        //Iterate through given matrix
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(grid[i][j]=='1')
+                {
+                    count++;
+                    rq.add(i);
+                    cq.add(j);
+                    grid[i][j]='0';
+                    //Processing the queue
+                    while(!rq.isEmpty())
+                    {
+                        int cr=rq.poll();
+                        int cc=cq.poll();
+                        
+                        for(int[] dir: dirs)
+                        {
+                            int nr=cr+dir[0];
+                            int nc=cc+dir[1];
+                            //Bound check
+                            if(nr>=0 && nc>=0 && nr<m && nc<n && grid[nr][nc]=='1')
+                            {
+                                rq.add(nr);
+                                cq.add(nc);
+                                grid[nr][nc]='0';
+                            }
+                        }
+                        
+                    }
+                    
+                }
+            }
+        }
+     return count;   
+    }
+    
+    
+}
