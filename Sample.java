f86d98a9ce1bf2a9b93ecbf72a487be14cbe0ed1
Prasad Gagarani <52399176+prasadgagarani@users.noreply.github.com>
2022-06-25 11:25:25 -0400

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,7 +1,57 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+//****NUMBER OF ISLAND-  DFS APPROACH****
+// Time Complexity : O(m*n);
+// Space Complexity :o(m*n)
+// Did this code successfully run on Leetcode :Y
+// Any problem you faced while coding this :N
 
+class Solution {
+    int [][] dirs;
+    int m;
+    int n; 
+    public int numIslands(char[][] grid) 
+    {
+        //Boiler plate code for going in 4 different directions
+        m=grid.length;
+        n=grid[0].length;  
+        
+        //Directions array      //u     //D  //L    //R
+        dirs=new int[][]{{-1,0},{1,0},{0,-1},{0,1}};
+        
+        int count=0;
+        //Iterate through given matrix
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(grid[i][j]=='1')
+                {
+                    count++;
+                    dfs(grid,i,j,m,n);
+                }
+            }
+        }
+     return count;   
+    }
+    
+    private void dfs(char[][] grid, int i, int j, int m, int n)
+    {
+        //Base
+        if(i==m || j==n || i<0 || j<0 || grid[i][j]=='0')
+        {
+            return;
+        }
+        
+        //Logic
+        grid[i][j]='0';
+        //Check if there is 1 in its other 4 directions
+        for(int[] dir: dirs)
+        {
+            int nr=dir[0]+i;
+            int nc=dir[1]+j;
+            
+            dfs(grid,nr,nc,m,n);
+        }
+    }
+}
 
 // Your code here along with comments explaining your approach
