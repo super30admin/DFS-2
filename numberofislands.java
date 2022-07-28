963b7967363f4e6bd7ba5c2abbad7e76589f5e0f
Shubham <shub01gupta@gmail.com>
2022-07-28 23:34:43 +0530

    Completed DFS-2

---
+++ numberofislands.java
@@ -0,0 +1,35 @@
+//TC :O(m*n)
+//SC :O(m*n)
+
+class Solution {
+    int total;
+    int[][] dirs;
+    int m,n;
+    public int numIslands(char[][] grid) {
+        if(grid == null || grid.length == 0)return 0;
+        //DFS
+        dirs = new int[][]{{-1,0},{1,0},{0,1},{0,-1}};//UDRL
+        m = grid.length;
+        n = grid[0].length;
+        for(int i = 0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j] == '1'){
+                    total++;
+                    dfs(i,j,grid);
+                }
+            }
+        }
+        return total;
+    }
+    private void dfs(int r, int c,char[][] grid){
+        //base
+        if(r<0 || c<0 || r==m || c==n || grid[r][c] != '1')return;
+        //logic
+        grid[r][c] = '0';
+        for(int[] dir:dirs){
+            int nr = dir[0]+r;
+            int nc = dir[1]+c;
+            dfs(nr,nc,grid);
+        }
+    }
+}
