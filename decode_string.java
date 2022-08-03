fa5f889cf44165b7d3118037eb3f4a9ecb0dd15b
siddbanerjee2012 <sidd.banerjee2012@gmail.com>
2022-08-02 22:14:17 -0500

    Completed Sprint DFS-2

---
+++ decode_string.java
@@ -0,0 +1,51 @@
+// Time Complexity : O(m*n)
+// Space Complexity : O(m*n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
+class Solution {
+    public String decodeString(String s) {
+        if(s==null || s.length()==0)
+        {
+            return"";
+        }
+        StringBuilder currString= new StringBuilder();
+        int num=0;
+        Stack<StringBuilder> strStack = new Stack<>();
+        Stack<Integer> numStack = new Stack<>();
+
+        for(int i=0;i<s.length();i++)
+        {
+            char c=s.charAt(i);
+            if(Character.isDigit(c))
+            {
+                num=num*10+c-'0';
+            }
+            else if(c=='[')
+            {
+                numStack.push(num);
+                strStack.push(currString);
+                currString=new StringBuilder();
+                num=0;
+            }
+            else if(c==']')
+            {
+                int times=numStack.pop();
+                StringBuilder newString= new StringBuilder();
+                for(int j=0;j<times;j++)
+                {
+                    newString.append(currString);
+                }
+                currString=strStack.pop().append(newString);
+            }
+            else
+            {
+                currString.append(c);
+            }
+        }
+        return currString.toString();
+
+    }
+} 
