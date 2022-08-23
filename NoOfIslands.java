3bc11f638749c11a81398e6f329a8a11acfde8f3
Jay Tanna <31876791+jaytanna61@users.noreply.github.com>
2022-08-23 19:28:16 -0400

    Update NoOfIslands.java

--- NoOfIslands.java
+++ NoOfIslands.java
@@ -1,14 +1,9 @@
 class Solution {
-    int[][] dirs;
-    int m; int n;
     public int numIslands(char[][] grid) {
         if(grid == null || grid.length == 0) return 0;
-        m =grid.length;
-        n = grid[0].length;
         int cnt = 0;
-        dirs = new int[][]{{0,1}, {0,-1}, {-1,0}, {1,0}};
-        for(int r = 0; r < m; r++) {
-            for(int c = 0; c < n; c++) {
+        for(int r = 0; r < grid.length; r++) {
+            for(int c = 0; c < grid[0].length; c++) {
                 if(grid[r][c] == '1') {
                     cnt++;
                     fillIslandDFS(grid, r, c); 
@@ -18,13 +13,12 @@ class Solution {
         return cnt;
     }
 
-    private void fillIslandDFS(char[][] grid, int r, int c) {
-        if(r < 0 || c < 0 || r == m || c == n || grid[r][c] == '0') return;
+    private void fillIslandDFS(char[][] grid, int r,int c) {
+        if(r < 0 || c < 0 || r == grid.length || c == grid[0].length || grid[r][c] == '0') return;
         grid[r][c] = '0';
-        for(int[] dir: dirs) {
-            int nr = r + dir[0];
-            int nc = c + dir[1];
-            fillIslandDFS(grid, nr, nc);
-        }
+        fillIslandDFS(grid, r ,c-1);
+        fillIslandDFS(grid, r, c+1);
+        fillIslandDFS(grid, r+1, c);
+        fillIslandDFS(grid, r-1, c);
     }
 } 
