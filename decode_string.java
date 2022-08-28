c99ccef507785f40484811880361d0c214fd22cf
vaibz96 <vshanbha@stevens.edu>
2022-08-27 19:27:29 -0700

    Number of Islands completed

--- decode_string.java
+++ decode_string.java
@@ -5,7 +5,7 @@
 
 
 // Your code here along with comments explaining your approach
-// The problem can be solved using two stacks or solved recurrsively, I have used 2 stack method where one stack is storing character value and the other stack is storing the number of time the character should be decoded. We need to iterate through the string and check at each character 4 conditions, if the charater is digit convert the digit into ascii value, then if the charater is opening square bracket then push the number character at the numstack and push the alphabet character at the string stack, if the character is closing bracket then decode the string, else just push the character into the current string 
+// The problem can be solved using two stacks or solved recurrsively, I have used 2 stack method where one stack is storing character value and the other stack is storing the number of time the character should be decoded. We need to iterate through the string and check at each character 4 conditions, if the charater is digit convert the digit into ascii value, then if the charater is opening square bracket then push the number character at the numstack and push the alphabet character at the string stack, if the character is closing bracket then decode the string else just push the character into the current string 
 
 
 
