92faf11e9b211bde3e016c17cefa6b1021399af5
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-01-22 11:23:20 -0800

    Create main.go

---
+++ decodeStr/main.go
@@ -0,0 +1,35 @@
+// time: o(nk^d)
+// n = len of s; k = max number; d = num of depths
+// space: o(numChars) + o(numOfNums) ; 2 stacks
+func decodeString(s string) string {
+    numSt := []int{}
+    parentSt := []*strings.Builder{}
+    
+    num := 0
+    curr := new(strings.Builder)
+    
+    for i := 0 ; i < len(s); i++ {
+        char := s[i]
+        if char >= '0' && char <= '9' {
+            num = num * 10 + int(char-'0')
+        } else if char == '[' {
+            numSt = append(numSt, num)
+            parentSt = append(parentSt, curr)
+            curr = new(strings.Builder)
+            num = 0
+        } else if char == ']' {
+            nTimes := numSt[len(numSt)-1]; numSt = numSt[:len(numSt)-1]
+            tmpBuilder := new(strings.Builder)
+            for i := 0; i < nTimes; i++ {
+                tmpBuilder.WriteString(curr.String())
+            }
+            parent := parentSt[len(parentSt)-1]; parentSt = parentSt[:len(parentSt)-1]
+            parent.WriteString(tmpBuilder.String())
+            curr = parent
+        } else {
+            curr.WriteByte(char)
+        }
+    }
+    
+    return curr.String()
+}
